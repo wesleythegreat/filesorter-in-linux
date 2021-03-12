a3fe1c0a8694422a92dc27a56d1af259211e38f0
wesleythegreat <51907689+wesleythegreat@users.noreply.github.com>
2021-03-12 10:15:08 +0800

    Update filesorter.py

--- filesorter.py
+++ filesorter.py
@@ -3,8 +3,8 @@ import os
 import shutil
 path = "/home/wesley/Diskdrive/Downloads/"
 names = os.listdir(path)
-folder_name = ['image','text','archive','pdf','docx','ppt','pythonfiles','jar','documents','iso','torrent']
-for x in range(0,11):
+folder_name = ['image','text','archive','pdf','docx','ppt','pythonfiles','jar','documents','iso','torrent','music','html','codefile','photoshop']
+for x in range(0,15):
     if not os.path. exists(path+folder_name[x]):
         os.makedirs(path+folder_name[x])
 for files in names:
@@ -61,4 +61,41 @@ for files in names:
 
     if ".torrent" in files and not os.path.exists(path+'torrent'+files):
         shutil.move(path+files, path+'torrent/'+files)
+
+    if ".mp3" in files and not os.path.exists(path + 'music' + files):
+        shutil.move(path + files, path + 'music/' + files)
+
+    if ".html" in files and not os.path.exists(path + 'html' + files):
+        shutil.move(path + files, path + 'html/' + files)
+
+    if ".ico" in files and not os.path.exists(path + 'image' + files):
+        shutil.move(path + files, path + 'image/' + files)
+
+    if ".gif" in files and not os.path.exists(path + 'image' + files):
+        shutil.move(path + files, path + 'image/' + files)
+
+    if ".deb" in files and not os.path.exists(path + 'archive' + files):
+        shutil.move(path + files, path + 'archive/' + files)
+
+    if ".img" in files and not os.path.exists(path + 'iso' + files):
+        shutil.move(path + files, path + 'iso/' + files)
+
+    if ".rar" in files and not os.path.exists(path + 'archive' + files):
+        shutil.move(path + files, path + 'archive/' + files)
+
+    if ".json" in files and not os.path.exists(path + 'codefile' + files):
+        shutil.move(path + files, path + 'codefile/' + files)
+
+    if ".xml" in files and not os.path.exists(path + 'html' + files):
+        shutil.move(path + files, path + 'html/' + files)
+
+    if ".psd" in files and not os.path.exists(path + 'photoshop' + files):
+        shutil.move(path + files, path + 'photoshop/' + files)
+
+    if ".sh" in files and not os.path.exists(path + 'codefile' + files):
+        shutil.move(path + files, path + 'codefile/' + files)
+
+    if ".tar" in files and not os.path.exists(path + 'archive' + files):
+        shutil.move(path + files, path + 'archive/' + files)
+
 print ("Done")
